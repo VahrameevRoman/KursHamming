1aef39aba17421d33ea6eeb38774f812e785cf65
Roman Vahrameev <romance.vah@mail.ru>
2024-05-23 01:02:07 +0300

    03

--- KursHamming/Form1.cs
+++ KursHamming/Form1.cs
@@ -123,12 +123,86 @@ namespace KursHamming
             if (firstHammingTextBox.Text == "") return;
 
             string hammingCode = firstHammingTextBox.Text;
+            string emptyHammingCode;
             int hammingLength = hammingCode.Length;
             int countControlBit;
+            int[,] hammingTable;
+            int[] controlBits;
+            int[] oldControlBits;
+            int[] locationControlBits;
+            int locationError;
+
             //считаем количество контрольных бит
             countControlBit = (int)Math.Ceiling(Math.Log(hammingLength + 1, 2));
-            //пересчитываем контрольные биты
+            controlBits = new int[countControlBit];
+            oldControlBits = new int[countControlBit];
+            locationControlBits = new int[countControlBit];
 
+            //заполнение таблицы хэмминга
+            hammingTable = new int[countControlBit, hammingLength];
+            for (int i = 0; i < hammingLength; i++)
+            {
+                //каждая строка это число в двоичной системе, написанное сверху вниз, оставшиеся клетки заполнены нулями автоматически
+                string column = Convert.ToString(i + 1, 2);
+                column = new string(column.Reverse().ToArray()); //переворот строки
+                for (int j = 0; j < column.Length; j++)
+                {
+                    hammingTable[j, i] = column[j] - '0';
+                }
+            }
+
+            //получаем уже имеющиеся контрольные биты
+            for (int i = 0; i < countControlBit; i++)
+            {
+                int tempLocation = (int)Math.Pow(2, i);
+                locationControlBits[i] = tempLocation;
+                oldControlBits[i] = hammingCode[tempLocation - 1] - '0';
+            }
+
+            //зануляем контрольные биты для их пересчёта
+            emptyHammingCode = hammingCode;
+            for (int i = 0; i < countControlBit; i++)
+            {
+                int tempLocation = locationControlBits[i] - 1;
+                emptyHammingCode = emptyHammingCode.Remove(tempLocation, 1).Insert(tempLocation, "0");
+            }
+
+            //пересчитываем нулевые контрольные биты
+            for (int i = 0; i < countControlBit; i++)
+            {
+                int temp = 0;
+                for (int j = 0; j < hammingLength; j++)
+                {
+                    temp += hammingTable[i, j] * (emptyHammingCode[j] - '0');
+                }
+                controlBits[i] = temp % 2;
+            }
+
+            // проверка на ошибку, ищем элемент с ошибкой
+            locationError = 0;
+            for (int i = 0; i < countControlBit; i++)
+            {
+                if (controlBits[i] != oldControlBits[i]) locationError += locationControlBits[i];
+            }
+            if (locationError == 0)  //ошибки нет
+            {
+                ErrorStatusTextBox.Text = "Ошибка не обнаружена";
+                return;
+            }
+            locationError--;
+
+            //замена ошибочного бита
+            if (hammingCode[locationError] == '0')
+            {
+                hammingCode = hammingCode.Remove(locationError, 1).Insert(locationError, "1");
+            }
+            else if (hammingCode[locationError] == '1')
+            {
+                hammingCode = hammingCode.Remove(locationError, 1).Insert(locationError, "0");
+            }
+            ErrorStatusTextBox.Text = "Обнаружена ошибка в разряде #" + (locationError+1) + ". Ошибка устранена.";
+            correctHammingTextBox.Text = hammingCode;
+            return;
         }
     }
 }
